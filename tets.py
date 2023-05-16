778a0215d7246b716b69e6ff90bc381cd70b485f
Kaplin_school_PC <littlebrotherprog@gmail.com>
2023-05-16 23:53:57 +0300

    accept messages from tg to vk is DONE

--- tets.py
+++ tets.py
@@ -1,69 +1,102 @@
 import vk_api
 import telebot
 import requests
+# import json
+import datetime
 from vk_api.longpoll import VkLongPoll, VkEventType
+from threading import Thread
 
-class vk:
-    
-    def __init__(self, message_tg):
-        self.message = message_tg
+# Класс отвечающий за работу с ВК
+
+class VK:
+
+    # Функция ицилизации 
+    def __init__(self):
+        # Требуется токен ПОЛУЧЯТЕЛЯ сообщений
         self.token = open('token_vk.txt').read()
+        # Авторизация в ВК
         self.vk_session = vk_api.VkApi(token= self.token)
-        self.tg = self.vk_session.get_api()
+        # Сохранения текущей сессии
+        self.vk = self.vk_session.get_api()
 
+    # Функция для поиски новых сообщений от конкретного пользователя
     def catch_messages(self):
         longpoll = VkLongPoll(self.vk_session)
         for event in longpoll.listen():
+            # Ловим новое сообщение и проверяем от кого оно
             if event.type == VkEventType.MESSAGE_NEW and event.user_id == 146246943:
+                # Вытаскиваем текст нужного нам сообщения и помешаем в переменную  
+                user = self.vk_session.method("users.get", 
+                                              {"user_ids": event.user_id}
+                                              )
+                full_name = user[0]['first_name'] + ' ' + user[0]['last_name']
                 message_for_tg = event.text
-                tg = TG(message_for_tg)
-                tg.accept_message()
-                print('accept')
+                # Пеобразование времени отправки сообщения в читаемое
+                value = datetime.datetime.fromtimestamp(event.timestamp)
+                time = value.strftime('%Y-%m-%d %H:%M:%S')
+                # Выводим в консоль дату и и время, от кого сообщение и текст сообщения
+                print('=' * 20,
+                      '\n',
+                      f'Дата и время отправки: {time}\n',
+                      f'Сообщение от: {full_name}\n', 
+                      f'Текст сообщения: {message_for_tg}',
+                    )
+                # Инцилизируем класс отвечающий за работу с Телеграммом
+                tg = TG()
+                # Активирцем функцию для отправки сообщения в Телеграмм
+                status = tg.accept_message(message_for_tg)
+                # Выводим в консоль статус кода и расшифровку
+                print(f'Код запроспа на отправку сообщения в тг: {status[0]}',
+                      '\n',
+                      f'Расшифровка кода запроса: {status[1]} \n',
+                      '=' * 20        
+                      )
     
-    def accept_message(self):
-        self.tg.message.send(user_id=146246943, message=self.message)
-
-
-
-                
+    def accept_message(self, message_tg, random_id):
+        print('message accept in vk')
+        self.vk.messages.send(user_id=146246943,random_id = random_id, message=message_tg)
 
 
 class TG:
 
-    def __init__(self, message_vk):
+    def __init__(self):
         self.token = open('token_tg.txt').read()
         self.bot = telebot.TeleBot(self.token, parse_mode=None)
-        self.message = message_vk
         self.url = 'https://api.telegram.org/bot'
         self.chat_id = '2134234148'
         @self.bot.message_handler()
         def catch_text(message):
             if(message.text != ''):
                 message_for_vk = message.text
-                VK = vk(message_for_vk)
-                send_message = VK.accept_message()
+                vk = VK()
+                random_id = message.id
+                send_message = vk.accept_message(message_for_vk, random_id)
+                print('accept')
                 
 
     def run(self):
         self.bot.polling()
 
-    def accept_message(self):
+    def accept_message(self, message_vk):
         url = self.url + self.token + '/sendMessage'
-        r = requests.post(url, data={ 
+        accept = requests.post(url, data={ 
                                     'chat_id': self.chat_id,
-                                    'text': self.message
+                                    'text': message_vk
                                     })
-        print(r.status_code)
-    
-    # def send_message(self):
-    #     url = self.url + self.token + '/search'
-    #     r = requests.get(url, data={ 
-    #                                 'from_id': self.chat_id,
-    #                                 })
-    #     print(r.status_code)
+        if accept.status_code != 200:
+            return [accept.status_code,
+                    accept.json()['description']
+                    ]
+        else:
+            return [accept.status_code,
+                    accept.reason
+                    ]
+
         
 
 
 if __name__ == '__main__':
-    tg = TG('l')
-    tg.run()
+    vk = VK()
+    tg = TG()   
+    Thread(target = lambda: tg.run()).start()
+    Thread(target = lambda: vk.catch_messages()).start()
