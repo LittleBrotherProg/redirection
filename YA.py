a97edaf17df13c9849af2d7894ea0d28177da14d
Kaplin_school_PC <littlebrotherprog@gmail.com>
2023-05-23 20:55:27 +0300

    send foto from to cloud YA

---
+++ YA.py
@@ -0,0 +1,95 @@
+import requests
+import json
+from progress.bar import ChargingBar
+from urllib.parse import urlencode
+
+
+class YA():
+
+
+    def __init__(self, 
+                url_photo, 
+                name_photo, 
+                size
+                ):
+        self.token = open('token_YA.txt').read()
+        self.url_photo = url_photo
+        self.headers = {
+                        'Content-Type': 'application/json', 
+                        'Authorization': self.token
+                        }
+        self.params = {'path':'disk:/'}
+        self.name_folder = 'Диплом_Сорокин'
+        self.name_photo = name_photo
+        self.size = size
+
+
+    def create_folder(self):
+        bar_create_folder = ChargingBar(
+            'Создание/Проверка папки для фото', 
+            max = 100
+            )
+        url = 'https://cloud-api.yandex.net/v1/disk/resources'
+        info_folder = requests.get(
+                                url, 
+                                headers={**self.headers}, 
+                                params={**self.params}
+                                )
+        count_files = len(info_folder.json()['_embedded']['items'])
+        bar_create_folder.next(25)
+        for count in range(int(count_files)):
+            bar_create_folder.next(25)
+            type_files = info_folder.json()['_embedded']['items'][count]['type']
+            count_name_folder = info_folder.json()['_embedded']['items'][count]['name']
+            if (type_files == 'dir'):
+                if (self.name_folder != count_name_folder):
+                    params = dict(
+                                path = 
+                                self.params.get('path') 
+                                + self.name_folder
+                                  )  
+                    requests.put(
+                                url, 
+                                headers={**self.headers}, 
+                                params={**params}
+                                )
+        bar_create_folder.finish()
+        return
+
+
+    def loading_profile_picture(self):
+        bar_loading_profile_picture = ChargingBar(
+            'Загрузка файла на диски', 
+            max = 100
+            )
+        bar_loading_profile_picture.next(25)
+        url_loading = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+        bar_loading_profile_picture.next(25)
+        params = dict(path = 
+                        self.params.get('path') 
+                        + self.name_folder 
+                        + '/' 
+                        + self.name_photo 
+                        + '.jpg', 
+                     url = self.url_photo
+                    )
+        bar_loading_profile_picture.next(25)
+        succsesful = requests.post( 
+                                    url_loading, 
+                                    params={**params}, 
+                                    headers={**self.headers}
+                                )
+        bar_loading_profile_picture.next(25)
+        info_file = {
+                    'file_name':self.name_photo 
+                    + '.jpg', 'size':self.size 
+                    }
+        bar_loading_profile_picture.finish()
+        return succsesful, info_file
+    
+    def dowland_photo(self):
+        url = 'https://cloud-api.yandex.net/v1/disk/resources/download?'
+        public_key = f'/{self.name_folder}/{self.name_photo}'
+        response = requests.get(url, path = public_key,  headers={**self.headers})
+        download_url = response.json()['href']
+        return download_url
--- tets.py
+++ tets.py
@@ -1,10 +1,14 @@
 import vk_api
+from vk_api.upload import VkUpload
+from vk_api.utils import get_random_id
+from vk_api.longpoll import VkLongPoll, VkEventType
 import telebot
 import requests
 # import json
 import datetime
-from vk_api.longpoll import VkLongPoll, VkEventType
 from threading import Thread
+from YA import *
+from io import BytesIO
 
 # Класс отвечающий за работу с ВК
 
@@ -18,6 +22,8 @@ class VK:
         self.vk_session = vk_api.VkApi(token= self.token)
         # Сохранения текущей сессии
         self.vk = self.vk_session.get_api()
+        self.peer_id = 146246943
+        self.upload = VkUpload(self.vk)
 
     # Функция для поиски новых сообщений от конкретного пользователя
     def catch_messages(self):
@@ -51,33 +57,86 @@ class VK:
                       f'Расшифровка кода запроса: {status[1]} \n',
                       '=' * 20        
                       )
-    
-    def accept_message(self, message_tg, random_id):
-        print('message accept in vk')
+    # Функция отправки сообщения из Телеграмма в Вк
+    def accept_message_text(self, message_tg, random_id):
+        # Отправка сообщения
         self.vk.messages.send(user_id=146246943,random_id = random_id, message=message_tg)
+        print('Отправка успешна',
+              '=' * 20)
+    
+    def upload_photo(self, url):
+        img = requests.get(url).content
+        f = BytesIO(img)
 
+        response = self.upload.photo_messages(f)[0]
+
+        owner_id = response['owner_id']
+        photo_id = response['id']
+        access_key = response['access_key']
+        attachment = f'photo{owner_id}_{photo_id}_{access_key}'
+        vk.messages.send(
+            random_id=get_random_id(),
+            peer_id=self.peer_id,
+            attachment=attachment
+        )
+# Класс отвечающий за работу с Телеграмом
 
 class TG:
 
+# Функция ицилизации 
     def __init__(self):
+        # Требуется токен БОТА сообщений
         self.token = open('token_tg.txt').read()
+        # Авторизация бота в Телеграмме
         self.bot = telebot.TeleBot(self.token, parse_mode=None)
+        # Ссылка для работы с api Телеграмма
         self.url = 'https://api.telegram.org/bot'
+        # id пользователя которуму будем адресовать сообщения
         self.chat_id = '2134234148'
-        @self.bot.message_handler()
-        def catch_text(message):
+        # Ловим сообщения которые пишем боту
+        @self.bot.message_handler(content_types=['text'])
+        def catch_text(chat_id,message):
             if(message.text != ''):
                 message_for_vk = message.text
                 vk = VK()
                 random_id = message.id
-                send_message = vk.accept_message(message_for_vk, random_id)
-                print('accept')
+                print('=' * 20,
+                      '\n',
+                      f'Время отправки \n',
+                      f'Сообщение от: {message.from_user.full_name} из ТГ \n',
+                      f'Текст сообщения: {message.text}',
+                      )
+                vk.accept_message_text(message_for_vk, random_id)
+        @self.bot.message_handler(content_types=['photo'])
+        def catch_photo(message):
+            file = self.bot.get_file(message.photo[-1].file_id)
+            # file_info = self.bot.get_file(file_id)
+            # downloaded_file = self.bot.download_file(file_info.file_path)
+            url = f'https://api.telegram.org/file/bot{self.token}/{file.file_path}'
+            load_photo = YA( 
+                    url,  
+                    str(message.photo[-1].file_unique_id), 
+                    message.photo[-1].file_size
+                    )
+            load_photo.create_folder()
+            load_photo.loading_profile_picture()
+            dowland_url = load_photo.dowland_photo()
+            vk = VK()
+            vk.upload_photo(dowland_url)
+
+            # with open(r'C:\Users\Kaplin\Desktop\photo_tg\lol.png','wb') as H:
+            #     H.write(downloaded_file)
+
+
+                
                 
 
     def run(self):
         self.bot.polling()
 
+    # Функция отправки сообщения от бота к пользователю
     def accept_message(self, message_vk):
+        # Сборка url для отправки запроса на отправку сообщения
         url = self.url + self.token + '/sendMessage'
         accept = requests.post(url, data={ 
                                     'chat_id': self.chat_id,
@@ -92,11 +151,12 @@ class TG:
                     accept.reason
                     ]
 
-        
 
-
+# Запуск программы
 if __name__ == '__main__':
+    # Инцилизация класса VK
     vk = VK()
+    # Инцилизация класса TG
     tg = TG()   
     Thread(target = lambda: tg.run()).start()
     Thread(target = lambda: vk.catch_messages()).start()
